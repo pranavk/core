a54500df0a8c6a9004c7f376c4462ab14c122c3f
matteocam <matteo.campanelli@gmail.com>
2015-06-02 19:34:22 -0400

    Added NonOverflowingText and logic to leave it in editing text
    Change-Id: I2cf4ad519917c80c51f9f693f9e27d5e3e655ffc

--- include/editeng/outliner.hxx
+++ include/editeng/outliner.hxx
@@ -50,6 +50,7 @@ class EditUndo;
 class ParagraphList;
 class OutlinerParaObject;
 class OverflowingText;
+class NonOverflowingText;
 class SvStream;
 class SvxBulletItem;
 class SvxFont;
@@ -773,7 +774,7 @@ public:
     void            SetParaRemovingHdl(const Link<>& rLink){aParaRemovingHdl=rLink;}
     Link<>          GetParaRemovingHdl() const { return aParaRemovingHdl; }
 
-    OutlinerParaObject *GetNonOverflowingParaObject() const;
+    NonOverflowingText *GetNonOverflowingText() const;
     OverflowingText *GetOverflowingText() const;
     void ClearOverflowingParaNum();
     void            DepthChangedHdl();
