8e64377d38bf56d21c25f6ff252c41f288aa18b0
matteocam <matteo.campanelli@gmail.com>
2015-06-16 16:30:08 -0400

    Exports (Non)OverflowingText classes
    Change-Id: I2a4b17e4d08d830cb413fef77af59ed9c031fb1f

--- svx/source/svdraw/svdotextdecomposition.cxx
+++ svx/source/svdraw/svdotextdecomposition.cxx
@@ -1626,8 +1626,6 @@ void SdrTextObj::impDecomposeChainedTextPrimitive(
 
     /* Begin overflow handling */
 
-    TextChain *aTxtChain = new TextChain;
-
     // If overflow occurs we have to cut the text at the right point
     // If in edit mode ImpEditEngine should have taken care of this
     if ( rOutliner.IsPageOverflow() && !IsInEditMode()) {
