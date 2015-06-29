3b7506276fda48898277837544394bb8de9f477d
matteocam <matteo.campanelli@gmail.com>
2015-06-29 03:55:31 -0400

    Using EditingTextChainFlow in editing mode
    Change-Id: Id49b9ed246e40436287e557b8218761f92127e49

--- svx/source/svdraw/svdotextdecomposition.cxx
+++ svx/source/svdraw/svdotextdecomposition.cxx
@@ -1644,7 +1644,7 @@ void SdrTextObj::impDecomposeChainedTextPrimitive(
     TextChainFlow aTxtChainFlow(const_cast<SdrTextObj*>(this));
     bool bIsOverflow;
 
-    aTxtChainFlow.CheckForFlowEvents(&rOutliner, NULL); // seconod parameter is to check whether you have to "prepare" the outliner's parameters
+    aTxtChainFlow.CheckForFlowEvents(&rOutliner);
 
     if (aTxtChainFlow.IsUnderflow() && !IsInEditMode())
     {
