56a09babc97594bde29f32bc8ca101c554028dca
Markus Mohrhard <markus.mohrhard@googlemail.com>
2015-07-15 21:28:07 +0200

    adapt code to last round of discussions
    Change-Id: Id0d2bae2a232f80ebe46cb55602c15c57b074e87

--- chart2/source/controller/sidebar/ChartElementsPanel.cxx
+++ chart2/source/controller/sidebar/ChartElementsPanel.cxx
@@ -271,8 +271,6 @@ ChartElementsPanel::ChartElementsPanel(
     get(mpCBLegend,  "checkbutton_legend");
     get(mpCBGridVertical,  "checkbutton_gridline_vertical");
     get(mpCBGridHorizontal,  "checkbutton_gridline_horizontal");
-    get(mpCBShowLabel,  "checkbutton_label");
-    get(mpCBTrendline,  "checkbutton_trendline");
 
     Initialize();
 }
@@ -302,8 +300,6 @@ void ChartElementsPanel::dispose()
     mpCBLegend.clear();
     mpCBGridVertical.clear();
     mpCBGridHorizontal.clear();
-    mpCBShowLabel.clear();
-    mpCBTrendline.clear();
 
     PanelLayout::dispose();
 }
